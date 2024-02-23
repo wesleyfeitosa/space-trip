10d8ff4cf89e3862faa760358dbacbff3943e631
Wesley Feitosa <jwesleydasilva@gmail.com>
2024-02-22 23:34:59 -0300

    refactor: setting environment vairables in .env.local

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,4 +1,4 @@
 export const serverUrl =
 	process.env.NODE_ENV === 'development'
-		? 'https://lldev.thespacedevs.com/2.2.0'
-		: 'https://ll.thespacedevs.com/2.2.0';
+		? process.env.SPACE_DEVS_API_URL_DEV
+		: process.env.SPACE_DEVS_API_URL_PROD;
