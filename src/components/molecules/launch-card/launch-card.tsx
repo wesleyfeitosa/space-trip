4dc93431d697af660e7183baab6d66549c9c21c3
Wesley Feitosa <jose.feitosa@vocedm.com.br>
2025-10-10 19:15:28 -0300

    feat: enhance launch card with link functionality and improved styling

--- src/components/molecules/launch-card/launch-card.tsx
+++ src/components/molecules/launch-card/launch-card.tsx
@@ -1,12 +1,9 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import { translateMissionDescription } from '@/utils/translations';
 import { type UpcomingLaunch } from '@/interfaces/upcoming-launch';
 import { ProgressiveImage } from '@/components/atoms/progressive-image/progressive-image';
 import { Countdown } from '@/components/atoms/countdown/countdown';
-import { Button } from '@/components/atoms/button/button';
 import { BadgeStatus } from '../badge-status/badge-status';
 import styles from './launch-card.module.css';
 
@@ -15,33 +12,27 @@ interface Props {
 }
 
 export function LaunchCard({ launch }: Props) {
-	const router = useRouter();
-
 	return (
-		<div className={styles.card}>
-			<ProgressiveImage
-				thumbnailUrl={launch.image?.thumbnail_url}
-				fullImageUrl={launch.image?.image_url}
-				alt={launch.name}
-				className={styles.imageContainer}
-				loading="lazy"
-			/>
-			<section className={styles.cardContent}>
-				<h3>{launch.name}</h3>
-				<p className={styles.description}>
-					{translateMissionDescription(launch.mission?.description ?? '')}
-				</p>
-				<section className={styles.statusContainer}>
-					<Countdown net={launch.net} />
-					<BadgeStatus status={launch.status} />
-				</section>
-				<Button
-					title="Ver detalhes"
-					onClick={() => {
-						router.push(`/launch/details/${launch.id}`);
-					}}
+		<Link href={`/launch/details/${launch.id}`} className={styles.cardLink}>
+			<article className={styles.card}>
+				<ProgressiveImage
+					thumbnailUrl={launch.image?.thumbnail_url}
+					fullImageUrl={launch.image?.image_url}
+					alt={launch.name}
+					className={styles.imageContainer}
+					loading="lazy"
 				/>
-			</section>
-		</div>
+				<section className={styles.cardContent}>
+					<h3>{launch.name}</h3>
+					<p className={styles.description}>
+						{translateMissionDescription(launch.mission?.description ?? '')}
+					</p>
+					<section className={styles.statusContainer}>
+						<Countdown net={launch.net} />
+						<BadgeStatus status={launch.status} />
+					</section>
+				</section>
+			</article>
+		</Link>
 	);
 }
